e74376870d9b6eefb585c0650a071c5ef203846c
Joel Irineu <irineu.joel01@gmail.com>
2021-03-07 20:24:15 -0300

    configuração do axios para receber dados da API

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import api from '../../services/api'
 
 import './styles.css'
 
@@ -7,64 +8,49 @@ import like from '../../assets/like.svg'
 import comment from '../../assets/comment.svg'
 import send from '../../assets/send.svg'
 
-function Feed(){
-    return(
-        <section id="post-list">
-            <article>
-                <header>
-                    <div className="user-info">
-                        <span>Joel Irineu</span>
-                        <span className="place">Fortaleza</span>
-                    </div>
+class Feed extends Component{
+    state = {
+      feed: [],  
+    }
 
-                    <img src={more} alt="Mais"/>
-                </header>
+    async componentDidMount(){
+        const response = await api.get('posts')
 
-                <img src="http://localhost:3333/files/dognho.jpg" alt=""/>
+        this.setState({feed: response.data})
+    }
 
-                <footer>
-                    <div className="actions">
-                        <img src={like} alt=""/>
-                        <img src={comment} alt=""/>
-                        <img src={send} alt=""/>
-                    </div>
-
-                    <strong>150 curtidas</strong>
-                    <p>
-                        Um post de um dognho muito fofo
-                        <span>#dognho #fofinho #oia</span>
-                    </p>
-                </footer>
-            </article>
-
-            <article>
-                <header>
-                    <div className="user-info">
-                        <span>Joel Irineu</span>
-                        <span className="place">Fortaleza</span>
-                    </div>
-
-                    <img src={more} alt="Mais"/>
-                </header>
-
-                <img src="http://localhost:3333/files/dognho.jpg" alt=""/>
-
-                <footer>
-                    <div className="actions">
-                        <img src={like} alt=""/>
-                        <img src={comment} alt=""/>
-                        <img src={send} alt=""/>
-                    </div>
-
-                    <strong>150 curtidas</strong>
-                    <p>
-                        Um post de um dognho muito fofo
-                        <span>#dognho #fofinho #oia</span>
-                    </p>
-                </footer>
-            </article>
-        </section>
-    )
+    render(){
+        return(
+            <section id="post-list">
+                <article>
+                    <header>
+                        <div className="user-info">
+                            <span>Joel Irineu</span>
+                            <span className="place">Fortaleza</span>
+                        </div>
+    
+                        <img src={more} alt="Mais"/>
+                    </header>
+    
+                    <img src="http://localhost:3333/files/dognho.jpg" alt=""/>
+    
+                    <footer>
+                        <div className="actions">
+                            <img src={like} alt=""/>
+                            <img src={comment} alt=""/>
+                            <img src={send} alt=""/>
+                        </div>
+    
+                        <strong>150 curtidas</strong>
+                        <p>
+                            Um post de um dognho muito fofo
+                            <span>#dognho #fofinho #oia</span>
+                        </p>
+                    </footer>
+                </article>
+            </section>
+        )
+    }
 }
 
 export default Feed
