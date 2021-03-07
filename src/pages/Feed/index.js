d5248e2f0695fb41e23b06bfc428080727425f79
Joel Irineu <irineu.joel01@gmail.com>
2021-03-07 20:41:41 -0300

    renderizando os dados da api

--- src/pages/Feed/index.js
+++ src/pages/Feed/index.js
@@ -22,32 +22,34 @@ class Feed extends Component{
     render(){
         return(
             <section id="post-list">
-                <article>
-                    <header>
-                        <div className="user-info">
-                            <span>Joel Irineu</span>
-                            <span className="place">Fortaleza</span>
-                        </div>
-    
-                        <img src={more} alt="Mais"/>
-                    </header>
-    
-                    <img src="http://localhost:3333/files/dognho.jpg" alt=""/>
-    
-                    <footer>
-                        <div className="actions">
-                            <img src={like} alt=""/>
-                            <img src={comment} alt=""/>
-                            <img src={send} alt=""/>
-                        </div>
-    
-                        <strong>150 curtidas</strong>
-                        <p>
-                            Um post de um dognho muito fofo
-                            <span>#dognho #fofinho #oia</span>
-                        </p>
-                    </footer>
-                </article>
+                { this.state.feed.map(post => (
+                    <article key={post._id}>
+                        <header>
+                            <div className="user-info">
+                                <span>{post.author}</span>
+                                <span className="place">{post.place}</span>
+                            </div>
+        
+                            <img src={more} alt="Mais"/>
+                        </header>
+        
+                        <img src={`http://localhost:3333/files/${post.image}`} alt=""/>
+        
+                        <footer>
+                            <div className="actions">
+                                <img src={like} alt=""/>
+                                <img src={comment} alt=""/>
+                                <img src={send} alt=""/>
+                            </div>
+        
+                            <strong>{post.likes} curtidas</strong>
+                            <p>
+                                {post.description}
+                                <span>{post.hashtags}</span>
+                            </p>
+                        </footer>
+                    </article>
+                )) }
             </section>
         )
     }
