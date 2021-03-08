a915345fc8b8d29c2dcd3096c8f33d6c57a2e074
Joel Irineu <irineu.joel01@gmail.com>
2021-03-08 00:45:17 -0300

    estrutura e estilização da pagina new

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,9 +1,68 @@
 import React, { Component } from 'react'
+import './styles.css'
 
 class New extends Component{
+    state = {
+        image: null,
+        author: '',
+        place: '',
+        description: '',
+        hashtags: '',
+    }
+
+    handleSubimit = e => {
+        e.preventDefault();
+
+        console.log(this.state)
+    }
+
+    handleImageChange = e => {
+        this.setState({ image: e.target.files[0] });        
+    }
+
+    handleChange = e => {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render(){
         return(
-            <h1>New</h1>
+            <form action="" id="new-post">
+                <input type="file" onChange={this.handleImageChange}/>
+
+                <input 
+                    type="text"
+                    name="author"
+                    placeholder="Autor do post"
+                    onChange={this.handleChange}
+                    value={this.setState.author}
+                />
+
+                <input 
+                    type="text"
+                    name="place"
+                    placeholder="local do post"
+                    onChange={this.handleChange}
+                    value={this.setState.place}
+                />
+
+                <input 
+                    type="text"
+                    name="description"
+                    placeholder="Descrição do post"
+                    onChange={this.handleChange}
+                    value={this.setState.description}
+                />
+
+                <input 
+                    type="text"
+                    name="hashtags"
+                    placeholder="Hashtags do post"
+                    onChange={this.handleChange}
+                    value={this.setState.hashtags}
+                />
+
+                <button type="submit" onClick={this.handleSubimit}>Enviar</button>
+            </form>
         )
     }
 }
